76ae9aab4e667b51203b205a75569a3a267bd150
Matias Surdi <matias.surdi@gmail.com>
2012-06-18 20:42:09 +0200

    Improved forwarder tests

--- spank/tests/test_forwarder.py
+++ spank/tests/test_forwarder.py
@@ -1,28 +1,28 @@
 import unittest
+import json
+import urllib2
+import time
+from mock import Mock,patch,call
 from StringIO import StringIO
 from spank.forwarder import Forwarder,ForwarderRequest
 
 SAMPLE_INPUT="level=notice\nfacility=syslog\nhost=hadron\ntime:isodate=2012-05-23T11:06:20\nprogram=syslog-ng\n message=syslog-ng shutting down; version='3.1.3'\nend\nquit"
-SAMPLE_OUTPUT='{"received": 1339062800527, "level": "notice", "facility": "syslog", "host": "hadron", "program": "syslog-ng", "time": "2012-05-23T11:06:20", "message": "syslog-ng shutting down; version=\'3.1.3\'"}1'
-
-class IncorrectOutputException(Exception):
-    pass
-
-class FakeForwarderRequest(ForwarderRequest):
-    def submit(self):
-        if self.payload != SAMPLE_OUTPUT:
-            raise IncorrectOutputException(self.payload)
+SAMPLE_OUTPUT='{"received": 1339062800527, "level": "notice", "facility": "syslog", "host": "hadron", "program": "syslog-ng", "time": "2012-05-23T11:06:20", "message": "syslog-ng shutting down; version=\'3.1.3\'"}'
 
 class TestForwarder(unittest.TestCase):
     def setUp(self):
         self.input_stream = StringIO()
         self.input_stream.write(SAMPLE_INPUT)
         self.input_stream.seek(0)
-        
-    def test_forward(self):
-        forwarder = Forwarder("http://localhost/",request_type=FakeForwarderRequest,input_stream=self.input_stream)
-        try:
-            forwarder.forward() 
-        except Exception,e:
-            self.fail(str(e))
+    
+    @patch("urllib2.urlopen",autospec=True)
+    @patch("time.time",autospec=True)
+    def test_forward(self,time_time_mock,urllib2_urlopen_mock):
+        time_time_mock.return_value=1339062800.527
+        forwarder = Forwarder("http://localhost/",input_stream=self.input_stream)
+        forwarder.forward() 
+        self.assertEqual(True,urllib2_urlopen_mock.called)
+        self.assertIn(call.read,urllib2_urlopen_mock.mock_calls)
+        self.assertEqual(urllib2_urlopen_mock.call_args[0][0].headers,{'Content-type': 'application/json'})
+        self.assertEqual(urllib2_urlopen_mock.call_args[0][0].data,SAMPLE_OUTPUT)
                    
--- spank/tests/test_rangefilter.py
+++ spank/tests/test_rangefilter.py
@@ -8,4 +8,4 @@ class TestRangeFilter(unittest.TestCase):
     def test_range(self):
         range_filter = index.RangeFilter(field="field1",from_="A",to="B")
         result = str(range_filter)
-        self.assertEqual('{"range": {"field1": {"to": "B", "from": "A"}}}',result)
+        self.assertEqual('{"range": {"field1": {"to": "B", "include_lower": true, "include_upper": true, "from": "A"}}}',result)
